e0f5cbef42d60d7e6cba32048e595151cd1c99d5
Francis8244 <60706869+Francis8244@users.noreply.github.com>
2020-10-19 17:53:54 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -3,6 +3,33 @@ public class Employee implements Comparable<Employee>{
 	public String name; 
 	public int time;
 	
+	public static int inputToInt(String time) {
+
+		if (time.contains(":")) {
+			String temp = time;
+			time = time.substring(0, time.indexOf(":"));
+			temp = temp.substring(temp.indexOf(":") + 1);
+			if (Integer.parseInt(time) > 24 || Integer.parseInt(time) < 0) {
+				System.out.println("Not a valid time. Please restart and try again.");
+				return -1;
+			}
+			if (Integer.parseInt(temp) > 59 || Integer.parseInt(temp) < 0) {
+				System.out.println("Not a valid time. Please restart and try again.");
+				return -1;
+			}
+			time += temp;
+		}
+		if (time.length() > 4 || time.length() < 3) {
+			System.out.println("Not a valid time. Please restart and try again.");
+			return -1;
+		}
+		try {
+			return Integer.parseInt(time);
+		} catch (NumberFormatException e) {
+			System.out.println("Not a valid time. Please restart and try again.");
+			return -1;
+		}
+	}
 	public Employee(String name, int time) {
 		this.name = name;
 		this.time = time;
